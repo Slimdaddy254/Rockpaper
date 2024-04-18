b6a8c490247b62939b8633ddb2827742db7a21d2
Shadrack Mutethia <shady@Shadracks-MacBook-Pro.local>
2024-04-18 13:00:09 +0300

    Create function to generate random  numbers between 0&2

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+function getComputerChoice() {
+    // Generate a random number between 0 and 2
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    // Assign rock, paper, or scissors based on the random number
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+const computerChoice = getComputerChoice();
+console.log("Computer's choice:", computerChoice);
+
